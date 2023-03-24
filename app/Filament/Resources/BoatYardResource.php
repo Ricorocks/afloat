53d9ea56f776d2e08f9e5ae49f635dfa0a8ea800
Richard <richard@ricorocks.agency>
2023-03-24 11:32:28 +0000

    Update BoatYard

---
+++ app/Filament/Resources/BoatYardResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BoatYardResource\Pages;
+use App\Filament\Resources\BoatYardResource\RelationManagers;
+use App\Models\BoatYard;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BoatYardResource extends Resource
+{
+    protected static ?string $model = BoatYard::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\TextInput::make('telephone_number')
+                    ->tel()
+                    ->required(),
+                Forms\Components\Select::make('marina_id')
+                    ->relationship('marina', 'name')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('telephone_number'),
+                Tables\Columns\TextColumn::make('marina.name'),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+                Tables\Actions\ForceDeleteBulkAction::make(),
+                Tables\Actions\RestoreBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBoatYards::route('/'),
+            'create' => Pages\CreateBoatYard::route('/create'),
+            'edit' => Pages\EditBoatYard::route('/{record}/edit'),
+        ];
+    }    
+    
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Filament/Resources/BoatYardResource/Pages/CreateBoatYard.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\BoatYardResource\Pages;
+
+use App\Filament\Resources\BoatYardResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBoatYard extends CreateRecord
+{
+    protected static string $resource = BoatYardResource::class;
+}
