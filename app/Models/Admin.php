9566e3cc3c6df3a9adfffccf201d8be9da428c89
Richard <richard@ricorocks.agency>
2023-05-13 23:19:08 +0100

    Add filament user

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,13 +2,20 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class Admin extends Authenticatable
+class Admin extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    public function canAccessFilament(): bool
+    {
+        return true;
+    }
+    
 }
