9e3176d62c29a79e932874d1363972813fc802a3
Richard <richard@ricorocks.agency>
2023-05-26 09:39:58 +0100

    Add fonts and logout

--- resources/views/layouts/public-statamic.blade.php
+++ resources/views/layouts/public-statamic.blade.php
@@ -5,8 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel User</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,700">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+        @livewireStyles
+        @livewireScripts
+        @stack('scripts')
     </head>
     <body>
         <div class="bg-white">
--- resources/views/pages/pagecomponents/loginswitcher.blade.php
+++ resources/views/pages/pagecomponents/loginswitcher.blade.php
@@ -1,15 +1,25 @@
 @if ($logon_type == 'marina')
     @if ($customer = Auth::guard('marinastaff')->user())
-    <a href="{{ route('logout') }}" class="text-sm font-semibold leading-6 text-white">Log Out</a> &nbsp; | &nbsp;
+    <a href="{{ route('logout') }}" class="text-sm font-semibold leading-6 text-white" onclick="event.preventDefault(); document.getElementById('logout-marina-form').submit();">
+        Log Out
+    </a> &nbsp; <span class="text-white">|</span> &nbsp;
     <a href="{{ route('marina-admin.pages.dashboard') }}" class="text-sm font-semibold leading-6 text-white">{{ $customer->name }}</a>
+    <form id="logout-marina-form" action="{{ route('marina-admin.logout') }}" method="POST" style="display: none;">
+        {{ csrf_field() }}
+    </form>
     @else
     <a href="{{ route('devmenu') }}" class="text-sm font-semibold leading-6 text-white">Log in <span
             aria-hidden="true">&rarr;</span></a>
     @endif
 @else
     @if ($customer = Auth::guard('web')->user())
-    <a href="{{ route('marina-admin.auth.login') }}" class="text-sm font-semibold leading-6 text-white">Log Out</a> &nbsp; | &nbsp;
+    <a href="{{ route('marina-admin.auth.login') }}" class="text-sm font-semibold leading-6 text-white"  onclick="event.preventDefault(); document.getElementById('logout-customer-form').submit();">
+        Log Out
+    </a> &nbsp; <span class="text-white">|</span> &nbsp;
     <a href="{{ route('user-profile-information.update') }}" class="text-sm font-semibold leading-6 text-white">{{ $customer->name }}</a>
+    <form id="logout-customer-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        {{ csrf_field() }}
+    </form>
     @else
     <a href="{{ route('devmenu') }}" class="text-sm font-semibold leading-6 text-white">Log in <span
             aria-hidden="true">&rarr;</span></a>
