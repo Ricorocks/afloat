554434e97bc2eb7e86e4cecac87a5f932868f1a3
Richard <richard@ricorocks.agency>
2023-06-02 13:31:54 +0100

    update to menu

--- resources/views/pages/pagecomponents/header.blade.php
+++ resources/views/pages/pagecomponents/header.blade.php
@@ -1,2 +1,5 @@
 {{-- @include('pages.pagecomponents.navmobilenew') --}}
-@include('pages.pagecomponents.navmobile')
+{{-- @include('pages.pagecomponents.navmobile') --}}
+<header class="absolute inset-x-0 top-0 z-50" >
+    @include('pages.pagecomponents.navbar')
+</header>
